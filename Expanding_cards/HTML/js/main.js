4676fd7ccebf45cac555c17764ea73688d77d310
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-02-11 17:36:53 +0000

    Add files via upload

---
+++ Expanding_cards/HTML/js/main.js
@@ -0,0 +1,23 @@
+// Author: Kyan Keise
+// Date:
+// Title: 
+
+
+
+const panels = document.querySelectorAll('.panel')
+
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+
+    })
+}
