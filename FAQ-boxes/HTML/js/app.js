4b455929ab5d6736a835371a44c55bb8ef62c2c9
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-07-29 13:57:58 +0100

    Add files via upload

---
+++ FAQ-boxes/HTML/js/app.js
@@ -0,0 +1,12 @@
+// Author: Kyan Keise
+// Date:
+// Title: 
+
+
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
