6eebe2300c72824a72d63145f000b5ccc8000966
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-09-19 18:05:18 +0100

    Add files via upload

---
+++ Split-landing-page/HTML/js/main.js
@@ -0,0 +1,17 @@
+// Author: Kyan Keise
+// Date: 30/05/22
+// Title: Split Landing Page 
+
+
+// Declared Variables 
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+// Event Listeners 
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
+
+// Event Listeners
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
