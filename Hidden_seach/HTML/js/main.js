a83a544c1eb67286cedc193c02a182ae7b6a2479
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-09-19 17:41:09 +0100

    Add files via upload

---
+++ Hidden_seach/HTML/js/main.js
@@ -0,0 +1,12 @@
+// Author: Kyan Keise
+// Date: Date: 30/05/2022
+// Title: Hidden Search widget 
+
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active');
+    input.focus();
+})
