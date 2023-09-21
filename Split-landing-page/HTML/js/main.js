31a8210a2f1cc1b8fb1d16a1990370190895eded
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-09-21 16:45:57 +0100

    Add files via upload

--- Split-landing-page/HTML/js/main.js
+++ Split-landing-page/HTML/js/main.js
@@ -1,17 +1,24 @@
 // Author: Kyan Keise
 // Date: 30/05/22
-// Title: Split Landing Page 
+// Title: Split Landing Page
 
-
-// Declared Variables 
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const container = document.querySelector('.container');
-
-// Event Listeners 
-left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
-left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
+// Declared Variables
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const container = document.querySelector(".container");
 
 // Event Listeners
-right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
-right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
+left.addEventListener("mouseenter", () =>
+  container.classList.add("hover-left")
+);
+left.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-left")
+);
+
+// Event Listeners
+right.addEventListener("mouseenter", () =>
+  container.classList.add("hover-right")
+);
+right.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-right")
+);
