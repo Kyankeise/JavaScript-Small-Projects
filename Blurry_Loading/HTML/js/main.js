515f2147f54ed72def0efb9f9c48a9f0653149a7
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-09-21 16:37:25 +0100

    Add files via upload

--- Blurry_Loading/HTML/js/main.js
+++ Blurry_Loading/HTML/js/main.js
@@ -1,31 +1,30 @@
 // Author: Kyan Keise
-// Date 21/06/22:
-// Title: blurry loading
+// Date: 21/06/22
+// Title: blurry loading Page
 
 // Create a function that will blur the background image
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
 
 // Create a variable that will set the number
-let load = 0
+let load = 0;
 
 // Variable that will set the interval
-let int = setInterval(blurring, 30)
+let int = setInterval(blurring, 30);
 
 // Create a function that will increase the number
 function blurring() {
-    load++
+  load++;
 
-
-    if (load > 99) {
-        clearInterval(int)
-    }
-    loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load, 0, 100, 20, 0)}px)`
+  if (load > 99) {
+    clearInterval(int);
+  }
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 20, 0)}px)`;
 }
 
 // Create a function that will scale the number
 function scale(number, inMin, inMax, outMin, outMax) {
-    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
