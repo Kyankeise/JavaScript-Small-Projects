c4bc86ff46807b70bac9f4be7fb37cf8ee2c27c8
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-09-21 16:44:38 +0100

    Add files via upload

--- Sound-board/HTML/js/main.js
+++ Sound-board/HTML/js/main.js
@@ -1,34 +1,34 @@
 // Author: Kyan Keise
-// Date: Date: 25/03/22 
-// Title: Soundboard project 
+// Date: Date: 25/03/22
+// Title: Soundboard project
 
 // Create a soundboard variable that plays audio clips on button press.
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 
 // forEach loop that will loop through the sounds array and create a button for each sound.
-sounds.forEach(sound => {
-    const btn = document.createElement('button');
-    btn.classList.add('btn');
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
 
-    btn.innerText = sound;
+  btn.innerText = sound;
 
-    // Event listener that will play the audio clip when the button is pressed.
-    btn.addEventListener('click', () => {
-        stopSongs();
+  // Event listener that will play the audio clip when the button is pressed.
+  btn.addEventListener("click", () => {
+    stopSongs();
 
-        // Play the audio clip.
-        document.getElementById(sound).play();
-    });
+    // Play the audio clip.
+    document.getElementById(sound).play();
+  });
 
-    document.getElementById('buttons').appendChild(btn);
-})
+  document.getElementById("buttons").appendChild(btn);
+});
 
 // Function that will stop the audio clips from overlapping.
 function stopSongs() {
-    sounds.forEach(sound => {
-        const song = document.getElementById(sound)
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
 
-        song.pause();
-        song.currentTime = 0;
-    })
+    song.pause();
+    song.currentTime = 0;
+  });
 }
