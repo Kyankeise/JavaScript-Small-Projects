b54ffa5ad7653a87fc182ff3714c674e857e5e97
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-09-21 16:46:52 +0100

    Add files via upload

--- form-wave-animation/HTML/js/main.js
+++ form-wave-animation/HTML/js/main.js
@@ -1,11 +1,16 @@
 // Author: Kyan Keise
-// Date: 25/03/22 
-// Title: Form wave Animation 
+// Date: 25/03/22
+// Title: Form wave Animation
 
+// declare Variable for label
+const labels = document.querySelectorAll(".form-control label");
 
-// declare Variable for label 
-const labels = document.querySelectorAll('.form-control label');
-
-labels.forEach(label => {
-    label.innerHTML = label.innerText.split('').map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`).join('');
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join("");
 });
